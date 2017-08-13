d39dfb3b5084ccbf62b6aff5ad1b096b3fb3c982
Julien Duseyau <julien.duseyau@gmail.com>
2017-08-13 18:27:01 +0200

    Preparing tests

---
+++ tests/AssetsTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maduser\Minimal\Assets\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AssetsTest extends TestCase
+{
+
+}
--- tests/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Maduser\Minimal\Assets\Tests;
-
-class ExampleTest extends \PHPUnit_Framework_TestCase
-{
-
-}
